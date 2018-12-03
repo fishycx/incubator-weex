105f17d45b606a8c18a9f6c40d1418e87881c762
fish <chuanxing@mamahao.com>
2018-12-03 19:55:37 +0800

    build

--- ios/sdk/WeexSDK/Sources/Utility/WXVersion.m
+++ ios/sdk/WeexSDK/Sources/Utility/WXVersion.m
@@ -20,8 +20,8 @@
 #import "WXVersion.h"
 #import "WXDefine.h"
 
-static const char* WeexSDKBuildTime = "2018-11-06 06:35:19 UTC";
-static const unsigned long WeexSDKBuildTimestamp = 1541486119;
+static const char* WeexSDKBuildTime = "2018-12-03 11:55:00 UTC";
+static const unsigned long WeexSDKBuildTimestamp = 1543838100;
 
 NSString* GetWeexSDKVersion(void)
 {
